a7b7ca44c0bb06f4a2023fb4b0c9b11808d83790
Rowan Parry <rowan.parry@hva.nl>
2019-03-18 14:26:44 +0100

    sql bestand gefixed

--- doc/users.sql
+++ doc/users.sql
@@ -1,1 +1,9 @@
-SELECT * FROM users.userlist;
+CREATE TABLE `userlist` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `givenname` varchar(45) NOT NULL,
+  `familyname` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `role` varchar(5) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
