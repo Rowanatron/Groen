469129a2df3ffa54b9337348a7b5b688616330df
Elmer <elmer_kralt@hotmail.com>
2019-03-19 12:07:16 +0100

    session toegevoegd aan login en landing page

--- app/public/systemoverview.php
+++ app/public/systemoverview.php
@@ -1,12 +1,31 @@
 <?php
+session_start();
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true){
+    if(isset($_SESSION["isAdmin"])){
+        // header("Location: systemoverview.php"); redirect naar Adminversie maken!
+        echo "Hallo, adminnetje";
+    } else {
+        // header("Location: systemoverview.php"); redirect naar Userversie maken!
+        echo "Hallo, usertje";
+    }
+    
+} else {
+    // errormessage dat de bezoeker hier niet zomaar mag komen!
+    header("Location: ../login.php");
+}
+
+
 require_once('../private/initialize.php');
 
 $page_title = 'Systemoverview';
 include(SHARED_PATH . '/header.php');
-?>
 
+
+
+?>
 <div id="content">
  <!-- Hier komt de content -->
 </div>
 
 <?php include(SHARED_PATH . '/footer.php')?>
+
