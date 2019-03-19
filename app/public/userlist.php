70b31f0a7be7a0d67deda515d7295ce2085eb17c
Anne Jan Sikkema <annejansikkema@gmail.com>
2019-03-19 11:13:47 +0100

    Userlist edited to include functions and pathConstants

--- app/public/userlist.php
+++ app/public/userlist.php
@@ -1,30 +1,17 @@
 
 <?php
-require_once('../private/initialize.php');
+require_once('../private/pathConstants.php');
+require_once('../private/functions.php');
 
 $page_title = 'Userlist';
 include(SHARED_PATH . '/header.php');
 include(PRIVATE_PATH . '/User.php');
 ?>
 
-<div id="content">
+<div id="content" class="container">
  <!-- Hier komt de content -->
 
 	<h2>Gebruikersoverzicht</h2>
-
-	<!-- temp styling -->
-	<style>
-		table, tr, th, td {
-			border-collapse: collapse;
-			border-width: 1px;
-			border-style: solid;
-		}
-		
-		td, th {
-			padding: 0.5rem;
-		}
-	</style>
-	<!-- end temp styling -->
 	
 	<table>
 		<tr>
@@ -40,7 +27,7 @@ include(PRIVATE_PATH . '/User.php');
 			include '../private/DatabasePDO.php';
 			$pdo = new DatabasePDO();
 			$conn = $pdo->get();
-			$query = "SELECT * FROM userlist";
+			$query = "SELECT * FROM userlist ORDER BY username;";
 			
 			try {
 				$statement = $conn->prepare($query);
@@ -52,7 +39,7 @@ include(PRIVATE_PATH . '/User.php');
 			$userArray = array();
 			
 			while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-				$user = new User($row['username'], $row['username'], $row['givenname'], $row['familyname'], $row['email'], $row['role']);
+				$user = new User($row['username'], $row['password'], $row['givenname'], $row['familyname'], $row['email'], $row['role']);
 				array_push($userArray, $user);
 			}	
 		?>
@@ -63,7 +50,7 @@ include(PRIVATE_PATH . '/User.php');
 			<td><?php echo $user->username ?></td>
 			<td><?php echo $user->givenname ?></td>
 			<td><?php echo $user->familyname ?></td>
-			<td><?php echo $user->admin ?></td>
+			<td><?php echo $user->role ?></td>
 			<td><a href="#edit-<?php echo $user->username ?>">Edit</a></td>
 			<td><a href="#delete-<?php echo $user->username ?>">Delete</a></td>
 		</tr>
