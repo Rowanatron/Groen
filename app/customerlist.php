fe330bf5cfc6e3ea39f80daf58946a0a828fbcbd
yvette <yvette.nikolov@gmail.com>
2019-03-25 11:37:57 +0100

    Enkele veranderingen

--- app/customerlist.php
+++ app/customerlist.php
@@ -1,14 +1,14 @@
 <!-- Default PHP header -->
 <?php
 
-require_once('../private/path_constants.php');
+require_once('private/path_constants.php');
 
 $page_title = 'Customerlist';
 
-require_once(PRIVATE_PATH . '/functions.php');
-require_once(PRIVATE_PATH . '/customer_functions.php');
-require_once(PRIVATE_PATH . '/Customer.php');
-require_once(PRIVATE_PATH . '/authorisation_functions.php');
+require_once('private/functions.php');
+require_once('customer_functions.php');
+require_once('Customer.php');
+require_once('private/authorisation_functions.php');
 
 session_start();
 
@@ -16,7 +16,7 @@ is_logged_in();
 session_expired();
 only_for_admins();
 
-include(SHARED_PATH . '/header.php');
+include('private/shared/header.php');
 
 
 // Code om klant te verwijderen
@@ -51,7 +51,7 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && ($_POST['action'] == 'delete_custo
 				<td>
 					<form action="customeredit" method="post">
 						<input type="hidden" name="customer_id" value="<?=$customer->customer_id; ?>"/>
-						<input type="image" name="submit" src="../public/img/edit_pencil.png" onmouseover="this.src='../public/img/edit-hover.png';" onmouseout="this.src='../public/img/edit_pencil.png';" border="0" alt="bewerk" style="width: 10%; height: 10%;" />
+						<input type="image" name="submit" src="public/img/edit_pencil.png" onmouseover="this.src='public/img/edit-hover.png';" onmouseout="this.src='public/img/edit_pencil.png';" border="0" alt="bewerk" style="width: 10%; height: 10%;" />
 					</form>
 				</td>
 				<td>
@@ -59,7 +59,7 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && ($_POST['action'] == 'delete_custo
 						<input type="hidden" name="action" value="delete_customer" />
 						<input type="hidden" name="customer_id" value="<?=$customer->customer_id; ?>" />
 						<input type="hidden" name="customer_name" value="<?=$customer->customer_name; ?>" />
-						<input type="image" src="../public/img/delete.png" onmouseover="this.src='../public/img/delete-hover.png';" onmouseout="this.src='../public/img/delete.png';"border="0" alt="delete" style="width: 7%; height: 7%;" />
+						<input type="image" src="public/img/delete.png" onmouseover="this.src='public/img/delete-hover.png';" onmouseout="this.src='public/img/delete.png';"border="0" alt="delete" style="width: 7%; height: 7%;" />
 					</form>
 				</td>
 				<!-- <td><a href="#edit-<? // =$user->username; ?>"></a><a href="#delete-<? // =$user->username; ?>"></a></td> -->
@@ -81,7 +81,7 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && ($_POST['action'] == 'delete_custo
 	</div>
 </div>
 
-<meta http-equiv="refresh" content="1801; ../public/login.php" />
+<meta http-equiv="refresh" content="1801; /public/login.php" />
 
 <!-- Default PHP footer -->
-<?php include(SHARED_PATH . '/footer.php')?>
+<?php include('private/shared/footer.php')?>
