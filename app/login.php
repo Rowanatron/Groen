7e3fe2bfe1514e25bac7a9c90579c35e3f4fa588
Bart Heijs <bartheijs@gmail.com>
2019-03-29 15:29:22 +0100

    favicaon op login pagina

--- app/login.php
+++ app/login.php
@@ -58,6 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" media="all" href="css/login.css">
     <title>Login</title>
